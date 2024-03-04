87f84713afced229ba7dbefa99cfd2d5cab80b99
yashu-7 <yaswanthreddypalle@gmail.com>
2024-03-04 20:50:26 +0530

    first lesson in rust

---
+++ basic_component_of_rust_code/target/debug/deps/basic_component_of_rust_code-03efa6e998d38723.d
@@ -0,0 +1,5 @@
+C:\Users\yaswa\learn\rust_files\learing-rust\rust_essentials\basic_component_of_rust_code\target\debug\deps\basic_component_of_rust_code-03efa6e998d38723.rmeta: src\main.rs
+
+C:\Users\yaswa\learn\rust_files\learing-rust\rust_essentials\basic_component_of_rust_code\target\debug\deps\basic_component_of_rust_code-03efa6e998d38723.d: src\main.rs
+
+src\main.rs:
